436416a2d61e473c36ceff5a04af0567e13bc0c4
Adam Hawke <ah14275@my.bristol.ac.uk>
2022-06-19 22:45:10 +0100

    Initial commit

---
+++ src/main/java/com/cgrecords/inventory/repository/InventoryRepository.java
@@ -0,0 +1,9 @@
+package com.cgrecords.inventory.repository;
+
+import com.cgrecords.inventory.model.InventoryItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface InventoryRepository extends JpaRepository<InventoryItem, Long> {
+}
