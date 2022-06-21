c969f088d33d28fbf378d94bcd8affff73fff70e
Adam Hawke <ah14275@my.bristol.ac.uk>
2022-06-21 13:05:36 +0100

    Added @GetMapping

--- src/main/java/com/cgrecords/inventory/controller/InventoryRestController.java
+++ src/main/java/com/cgrecords/inventory/controller/InventoryRestController.java
@@ -2,7 +2,9 @@ package com.cgrecords.inventory.controller;
 
 import com.cgrecords.inventory.model.InventoryItem;
 import com.cgrecords.inventory.service.InventoryService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +22,16 @@ public class InventoryRestController {
     @GetMapping(value = "/items", produces = "application/json")
     public List<InventoryItem> getInventory() {
         return inventoryService.getAllInventoryItems();
+    }
+
+    @GetMapping(value = "/items/{id}", produces = "application/json")
+    public ResponseEntity<InventoryItem> getInventoryItemByID(@PathVariable Long id) {
+        InventoryItem item = inventoryService.getInventoryItemByID(id);
+        if (item == null) {
+            return ResponseEntity.notFound().build();
+        } else {
+            return ResponseEntity.ok()
+                    .body(item);
+        }
     }
 }
